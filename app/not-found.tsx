9e4f7d006031b83f1b25c811a19b52a3a149e4a1
Dismas Banda <bandadismas@gmail.com>
2025-07-08 18:30:40 +0200

    style: reformat the page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
     title: "Ooops! Page not found",
@@ -7,6 +8,15 @@ export const metadata: Metadata = {
 
 export default function Page() {
     return (
-        <div>Page not found</div>
+        <div className="flex justify-center items-center flex-col h-[50dvh]">
+            <h1 className="text-5xl font-semibold py-10">404 Not Found</h1>
+            <p>Your visited page not found. You can return to the home page.</p>
+            <Link 
+                href={'/'}
+                className="bg-tertiary py-2 px-10 text-white mt-10 rounded-lg shadow-lg"
+            >
+                Back to home page
+            </Link>
+        </div>
     )
 }
