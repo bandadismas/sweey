c024f9e3034cf897c86577c7c94f851e3de6e076
Dismas Banda <bandadismas@gmail.com>
2025-07-08 12:02:35 +0200

    Add not found skeleton

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Ooops! Page not found",
+    description: "Sweey is the best E-Commerce Site that you could ever ask for."
+}
+
+export default function Page() {
+    return (
+        <div>Page not found</div>
+    )
+}
